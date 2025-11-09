d94f36d223b9e889e975614c6563595bb52e0091
Caby <cabydelgado@hotmail.es>
2025-11-09 18:45:11 +0100

    Player movement + dash
    Player can move using WASD, arrows or controller and dash with space bar or South button on controller.

---
+++ My project/Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,70 @@
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 RawMoveInput { get; private set; }
+    public bool DashInput { get; private set; }
+    public Coroutine DashCoroutine { get; private set; }
+    [SerializeField]
+    private float inputHoldTime = 0.2f;
+
+    public InputControl inputDevice { get; private set; }
+    public bool UsingController { get; private set; }
+    public bool RollInput { get; private set; }
+    private PlayerInput PlayerInpt { get; set; }
+    private InputActionMap GameplayMap { get; set; }
+    private InputActionMap MenuMap { get; set; }
+
+    private void Awake()
+    {
+        PlayerInpt = GetComponent<PlayerInput>();
+
+        GameplayMap = PlayerInpt.actions.FindActionMap("Gameplay");
+        MenuMap = PlayerInpt.actions.FindActionMap("Menu");
+    }
+
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        RawMoveInput = context.ReadValue<Vector2>();
+        MoveInput = RawMoveInput.normalized;
+    }
+
+    public void OnDashInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            SetDash(true);
+            if (DashCoroutine != null)
+                StopCoroutine(DashCoroutine);
+            DashCoroutine = StartCoroutine(StartDashHoldTime());
+        }
+    }
+
+    public void SetDash(bool dash)
+    {
+        DashInput = dash;
+    }
+
+    private IEnumerator StartDashHoldTime()
+    {
+        yield return new WaitForSeconds(inputHoldTime);
+        DashInput = false;
+    }
+}
---
+++ My project/Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public PlayerInputHandler inputHandler;
+    private Vector2 movement = Vector2.right;
+    public float moveSpeed = 50f;
+    public float dashImpulse = 150f;
+    public float dashCooldown = 2f;
+    private bool dashReady;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputHandler = GetComponent<PlayerInputHandler>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dashReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputHandler.MoveInput != Vector2.zero)
+        {
+            movement = inputHandler.MoveInput;
+        }
+
+        rb.AddForce(movement * moveSpeed);
+
+        if (dashReady && inputHandler.DashInput)
+        {
+            StartDash();
+        }
+
+    }
+
+    private void StartDash()
+    {
+        rb.AddForce(movement * dashImpulse, ForceMode2D.Impulse);
+        StartCoroutine(StartDashCD());
+        inputHandler.SetDash(false);
+    }
+
+    private IEnumerator StartDashCD()
+    {
+        dashReady = false;
+        yield return new WaitForSeconds(dashCooldown);
+        dashReady = true;
+    }
+}
